fc93a68517e5ddbd7d33152fffe2ea0938249c25
Lucas Soares <lsoares267@gmail.com>
2016-08-03 08:44:54 -0300

    Raiz
    Trabalho de Raiz quadrada

---
+++ Raiz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiz
+{
+    class Program
+    {
+        public static int numeroEscolhido;
+        static void Main(string[] args)
+        {
+        
+          numeroEscolhido = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= numeroEscolhido; i +=2)
+                {
+                    numeroEscolhido -= i;
+                }
+
+            if (numeroEscolhido <= 0)
+                {
+                    Console.WriteLine("É raiz perfeita!");
+                   //Console.WriteLine(numeroEscolhido);
+                    Console.ReadKey();
+                }
+            else
+                {
+                    Console.WriteLine("A raiz não é perfeita!");
+                    Console.ReadKey();
+                }
+        }
+    }
+}
